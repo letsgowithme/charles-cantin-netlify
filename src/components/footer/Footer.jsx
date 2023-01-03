e4a2b212a7b4659ca89fdb91603d2350d0887129
iryna <musicira@gmail.com>
2023-01-03 19:47:51 +0100

    copyright date correction

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="footer footer-color">
      <WebIcons />
 
-      <div className="copyright">copyright 2022 Iryna Guenin</div>
+      <div className="copyright">copyright 2023 Iryna Guenin</div>
     </div>
   );
 };
