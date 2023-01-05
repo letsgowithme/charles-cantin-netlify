0737a5dc678a3559f7ba73fe2d774376eb39a006
iryna <musicira@gmail.com>
2023-01-05 11:01:37 +0100

    Footer et Mensions Legales correction

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,14 +6,12 @@ import "./footer.css";
 const Footer = () => {
   return (
     <div className="footer footer-color">
-      <WebIcons className="footer-item"/>
+      <WebIcons className="footer-item" />
       <div className="footer-items">
         <Link to="/mentionsLegales" className="mentionsLegales footer-item">
           Mentions Legales
         </Link>
-
         <div className="copyright footer-item">copyright@2023 Iryna Guenin</div>
-
         <Link to="/contact" className="footer-contact footer-item">
           Contact
         </Link>
