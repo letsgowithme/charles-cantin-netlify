654ec8de5147c6fbe8a9293a3f13acc1140ea836
letsgowithme <83494545+letsgowithme@users.noreply.github.com>
2023-12-09 08:28:05 +0100

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,13 +7,21 @@ const Footer = () => {
   return (
     <div className="footer">
       <WebIcons className="footer-item" />
+      <div className="footer-line">
       <div className="footer-items">
         <Link to="/mentionsLegales" className="footer-item mentionsLegales">
           Mentions Legales
         </Link>
+        </div>
         <div className="footer-item copyright">copyright&copy;2023 Iryna Guenin</div>
-      </div>
-    </div>
+     
+      <div className="footer-items">
+        <Link to="/policyPrivacy" className="footer-item mentionsLegales">
+        Politique de confidentialité
+        </Link>
+        </div>
+        </div>
+        </div>
   );
 };
 
